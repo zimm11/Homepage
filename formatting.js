0111c853a4f6560b8363678fec86e016c70f60d3
Chris Zimmerman <cdzimm118@gmail.com>
2019-09-13 17:41:56 -0400

    Major design changes

--- formatting.js
+++ formatting.js
@@ -1,11 +1,40 @@
 $(document).ready(function(){
-  $("button").click(function(){
-    if ($("button").text == "Menu ☰"){
-      $("button").text("x");
+  $("#menu").click(function(){
+    if ($("#menu").text == "Menu ☰"){
+      $("#menu").text("x");
     } else {
-      $("button").text("Menu ☰");
+      $("#menu").text("Menu ☰");
     }
     $("ul").toggle("slow");
   });
 });
 
+
+$(document).ready(function(){
+  $("#hide").click(function(){
+    $("#container").hide(500);
+  });
+  $("#hide").click(function(){
+    $("#about-me").show(500);
+  });
+});
+
+
+$(document).ready(function(){
+  $("#image").click(function(){
+    $("#container").hide(500);
+  });
+  $("#image").click(function(){
+    $("#about-me").show(500);
+  });
+});
+
+
+$(document).ready(function(){
+  $("#show").click(function(){
+    $("#about-me").hide(500);
+  });
+  $("#show").click(function(){
+    $("#container").show(500);
+  });
+});
